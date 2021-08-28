1411560e91674461a81710a22b86a68f539c7fc1
AhmedAliAfifii <87272412+AhmedAliAfifii@users.noreply.github.com>
2021-08-28 22:36:32 +0200

    Update main.c

--- main.c
+++ main.c
@@ -5,44 +5,74 @@
 #define MAX 7
 int intArray[MAX] = {38,27,43,3,9,82,10};
 int b[MAX];
-void selectionSort(){
-	int i,j,indexMin;
-	for(i=0;i<MAX-1;i++){
-		indexMin=i;
-		for(j=i+1;j<MAX;j++){
-			if(intArray[j] < intArray[indexMin]){
-				indexMin=j;
-			}
-			
-		}
-		if(indexMin != i){
-			int temp= intArray[indexMin];
-			intArray[indexMin]=intArray[i];
-			intArray[i]=temp;
-		}
-	}
-}
-void bubbleSort(){
-	int temp;
-	int i,j;
-	int swapped =0;
-	for(i=0;i<MAX-1;i++){
-		
-		for(j=0;j<MAX-1-i;j++){
-			if(intArray[j] > intArray[j+1]){
-				temp = intArray[j];
-				intArray[j]=intArray[j+1];
-				intArray[j+1]=temp;
-				swapped = 1;
-			}
-		}
-		if(swapped != 0){
-			printf("swapped!");
-		}
-	}
-}
 
 
+// Merges two subarrays of arr[].
+// First subarray is arr[l..m]
+// Second subarray is arr[m+1..r]
+void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+ 
+    /* create temp arrays */
+    int L[n1], R[n2];
+ 
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+ 
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0; // Initial index of first subarray
+    j = 0; // Initial index of second subarray
+    k = l; // Initial index of merged subarray
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+ 
+    /* Copy the remaining elements of L[], if there
+    are any */
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+ 
+    /* Copy the remaining elements of R[], if there
+    are any */
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+ 
+/* l is for left index and r is right index of the
+sub-array of arr to be sorted */
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r) {
+       
+        int m = l + (r - l) / 2;
+ 
+        // Sort first and second halves
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+ 
+        merge(arr, l, m, r);
+    }
+}
 
 void display() {
    int i;
@@ -55,15 +85,85 @@ void display() {
 	
    printf("]\n");
 }
+void selectionSort() {
+   int indexMin,i,j;
+	
+   // loop through all numbers 
+   for(i = 0; i < MAX-1; i++) { 
+	
+      // set current element as minimum 
+      indexMin = i;
+		
+      // check the element to be minimum 
+      for(j = i+1;j < MAX;j++) {
+         if(intArray[j] < intArray[indexMin]) {
+            indexMin = j;
+         }
+      }
 
+      if(indexMin != i) {
+         printf("Items swapped: [ %d, %d ]\n" , intArray[i], intArray[indexMin]); 
+			
+         // swap the numbers 
+         int temp = intArray[indexMin];
+         intArray[indexMin] = intArray[i];
+         intArray[i] = temp;
+      }          
+
+      printf("Iteration %d#:",(i+1));
+      display();
+   }
+}  
+void bubbleSort() {
+   int temp;
+   int i,j;
+	
+   int swapped = 0;
+   
+   // loop through all numbers 
+   for(i = 0; i < MAX-1; i++) { 
+      swapped = 0;
+		
+      // loop through numbers falling ahead 
+      for(j = 0; j < MAX-1-i; j++) {
+         printf("     Items compared: [ %d, %d ] ", intArray[j],intArray[j+1]);
+
+         // check if next number is lesser than current no
+         //   swap the numbers. 
+         //  (Bubble up the highest number)
+			
+         if(intArray[j] > intArray[j+1]) {
+            temp = intArray[j];
+            intArray[j] = intArray[j+1];
+            intArray[j+1] = temp;
+
+            swapped = 1;
+            printf(" => swapped [%d, %d]\n",intArray[j],intArray[j+1]);
+         } else {
+            printf(" => not swapped\n");
+         }
+			
+      }
+
+      // if no number was swapped that means 
+      //   array is sorted now, break the loop. 
+      if(!swapped) {
+         break;
+      }
+      
+      printf("Iteration %d#: ",(i+1)); 
+      display();
+   }
+	
+}
 
 void main(void) {
    printf("Input Array: ");
    display();
-   //mergeSort(intArray, 0, MAX - 1);
+   mergeSort(intArray, 0, MAX - 1);
    //selectionSort();
-   bubbleSort();
-   //printf("Output Array: ");
+   //bubbleSort();
+   printf("Output Array: ");
    display();
 	
 }
